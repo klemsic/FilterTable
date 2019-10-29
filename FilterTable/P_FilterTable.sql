8c86a806ae7dd9a46cc7e9d7d2122aaec527ca88
Tomáš Klemsa <32998371+klemsic@users.noreply.github.com>
2019-10-29 01:29:42 +0100

    Random generate temporary table name.

--- FilterTable/P_FilterTable.sql
+++ FilterTable/P_FilterTable.sql
@@ -6,9 +6,30 @@
 	@TotalRows INT OUTPUT
 AS
 BEGIN
+	------------------------------ Prepare and check imput. ------------------------------
+	-- @TableName validation
+	IF(LEFT(@TableName,1) = '[' AND RIGHT(@TableName,1) = ']')
+	BEGIN
+		SET @TableName = SUBSTRING(@TableName,2,LEN(@TableName)-2)
+	END
+
+	-- @PageSize check
+	IF(@PageSize <= 0)
+	BEGIN
+		RAISERROR ('Invalid parameter: @PageSize must be a positive number.', 18, 0)
+		RETURN
+	END
+
+	-- @PageIndex check
+	IF(@PageIndex < 0)
+	BEGIN
+		RAISERROR ('Invalid parameter: @PageSize must be zero or greater than zero.', 18, 0)
+		RETURN
+	END
+	
 	------------------------------ Prepare where clause. ------------------------------
 	DECLARE @WhereClause AS NVARCHAR(MAX);
-	SELECT 'AND ' + [Column] + CASE [FilterType]	WHEN 'contains' THEN ' LIKE ''%' + [Filter] + '%'''
+	SELECT 'AND ' + [Column] + CASE [FilterType]	WHEN 'contains' THEN ' LIKE ''%' + REPLACE([Filter],'''','''''') + '%'''
 													WHEN 'notContains' THEN ' NOT LIKE ''%' + REPLACE([Filter],'''','''''') + '%'''
 													WHEN 'equals' THEN ' = ''' + REPLACE([Filter],'''','''''') + ''''
 													WHEN 'notEquals' THEN ' != ''' + REPLACE([Filter],'''','''''') + ''''
@@ -42,9 +63,11 @@ BEGIN
 
 	------------------------------ Filters the whole table. ------------------------------
 	DECLARE @IndexColumn AS NVARCHAR(255)
-	DECLARE @FilterSelect AS NVARCHAR(MAX)
 	SET @IndexColumn = CAST(NEWID() AS nvarchar(255))
-	SET @FilterSelect =	'SELECT ROW_NUMBER() OVER (Order by (SELECT 1)) AS [' + @IndexColumn + '], * INTO ##Data FROM ' + QUOTENAME(@TableName) + ' ' +
+	DECLARE @FilterSelect AS NVARCHAR(MAX)
+	DECLARE @ResultTable AS NVARCHAR(255)
+	SET @ResultTable = QUOTENAME('##' + CAST(NEWID() AS nvarchar(255)))
+	SET @FilterSelect =	'SELECT ROW_NUMBER() OVER (Order by (SELECT 1)) AS [' + @IndexColumn + '], * INTO ' + @ResultTable + ' FROM ' + QUOTENAME(@TableName) + ' ' +
 						'WHERE 1 = 1 ' + ISNULL(@WhereClause,'') + ' ' +
 						'ORDER BY ' + ISNULL(@OrderByClause,1)
 
@@ -68,19 +91,24 @@ BEGIN
 
 	-- Deletes records that are not on the page.
 	DECLARE @PaggingDelete AS NVARCHAR(MAX)
-	SET @PaggingDelete = 'DELETE FROM ##Data WHERE [' + @IndexColumn + '] NOT BETWEEN ' + CAST(@PageLowerBound AS NVARCHAR(100)) + ' AND ' + CAST(@PageUpperBound AS nvarchar(100))
+	SET @PaggingDelete = 'DELETE FROM ' + @ResultTable + ' WHERE [' + @IndexColumn + '] NOT BETWEEN ' + CAST(@PageLowerBound AS NVARCHAR(100)) + ' AND ' + CAST(@PageUpperBound AS nvarchar(100))
 	EXEC(@PaggingDelete)
 
 	-- Drop index for sorting records.
 	DECLARE @IndexDrop AS NVARCHAR(MAX)
-	SET @IndexDrop = 'ALTER TABLE ##Data DROP COLUMN [' + @IndexColumn + ']'
+	SET @IndexDrop = 'ALTER TABLE ' + @ResultTable + ' DROP COLUMN [' + @IndexColumn + ']'
 	EXEC(@IndexDrop)
 
 
 	------------------------------ Finally ------------------------------
+	-- Select result.
 	DECLARE @SelectPage AS NVARCHAR(MAX)
-	SET @SelectPage = 'SELECT * FROM ##Data ORDER BY ' + ISNULL(@OrderByClause,1)
+	SET @SelectPage = 'SELECT * FROM ' + @ResultTable + ' ORDER BY ' + ISNULL(@OrderByClause,1)
 	EXEC(@SelectPage)
 
-	DROP TABLE ##Data
+	-- Drop tmp table.
+	DECLARE @DropStatement AS NVARCHAR(MAX)
+	SET @DropStatement = 'DROP TABLE ' + @ResultTable
+	EXEC(@DropStatement)
+
 END
